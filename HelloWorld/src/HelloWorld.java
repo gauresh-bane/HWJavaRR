6e5f041d70aa2190441dad6059e1b6bff24f8662
Gauresh Bane <gaureshbane@gmail.com>
2020-03-22 08:29:38 +0530

    Initial Commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+/**
+ * @author gauresh_bane
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * 
+	 */
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+
+			System.out.println("Hello 1!");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world!");
+
+	}
+
+}
